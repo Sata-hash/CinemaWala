331717bea36913a9e3d99e5cd857094a8d5dd824
Sata-hash <satavisa1234@gmail.com>
2024-01-01 18:49:23 +0530

    Watch

---
+++ client/src/api/index.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+// const API = axios.create({baseURL:'http://localhost:5000'});
+// const API = axios.create({baseURL:'https://binger1.herokuapp.com'});
+const API = axios.create({baseURL:'https://binger-ec26.onrender.com'});
+
+
+API.interceptors.request.use((req)=>{
+    if(localStorage.getItem('profile')){
+        req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem('profile')).token}` ;
+    }
+    return req;
+});
+
+export const addFav = ({id,poster,title,date,media_type,vote_average}) => API.patch(`/user/addFav/${id}/${poster}/${title}/${date}/${media_type}/${vote_average}`);
+export const getUser = () => API.get(`/users`);
+
+export const signIn = (formData) => API.post('/user/signin',formData);
+export const signUp = (formData) => API.post('/user/signup',formData);
